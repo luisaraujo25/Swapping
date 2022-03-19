29f7ce6a6cead4e4774e1830a21c2fa9a4f9208b
Luísa Araújo <lmpa.pt@gmail.com>
2022-03-19 12:22:19 +0000

    setup pt 3, app created

--- src/MyScripts/install.sh
+++ src/MyScripts/install.sh
@@ -1,5 +1,12 @@
 #FOR WSL (UBUNTU)
 
-echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
+#echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 sudo apt install pipenv
 pipenv install django
+
+#CREATE A PROJECT
+#pipenv shell
+#django-admin startproject Swapping
+
+#CREATE AN APPLICATION
+#django-admin startapp app
