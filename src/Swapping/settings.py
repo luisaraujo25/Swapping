3fc7f4acc3901d9f8b82aa84072a3a0451c16137
Luísa Araújo <lmpa.pt@gmail.com>
2022-04-04 00:39:12 +0100

    css working again i think

--- src/Swapping/settings.py
+++ src/Swapping/settings.py
@@ -122,9 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, '../app/static'),
+]
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
