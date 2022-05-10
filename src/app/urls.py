74c0c14861abeb993f40949a56444490a7cc51cd
Luísa Araújo <lmpa.pt@gmail.com>
2022-05-10 15:28:55 +0100

    urls changed

--- src/app/urls.py
+++ src/app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('request/', views.request, name = 'make request'),
     path('viewrequest/<int:idReq>/', views.viewrequest, name = 'view request'),
     path('import/', views.importData, name = 'import data'),
-    path(r'^confirm/(?P<ridb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.confirmRequest1, name = 'confirmRequest1'),
-    path(r'^confirm/(?P<ridb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-{1,20})/$', views.confirmRequest2, name = 'confirmRequest2'),
+    path(r'^confirm/(?P<ridb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]*{1,20})/$', views.confirmRequest1, name = 'confirmRequest1'),
+    path(r'^confirm/(?P<ridb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]*{1,20})/$', views.confirmRequest2, name = 'confirmRequest2'),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico')))
 ]
