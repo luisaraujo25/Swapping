14e38383b00d6d9463f589f1ebc058619c1ba1af
Luísa Araújo <lmpa.pt@gmail.com>
2022-06-10 00:26:00 +0100

    changed settings, css working i think

--- src/Swapping/settings.py
+++ src/Swapping/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,17 +134,18 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, '../app/static'),
-]
 STATIC_URL = 'app/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 
 STATICFILES_FINDERS = (
 'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 'django.contrib.staticfiles.finders.FileSystemFinder',
 )
+
+print("base: ", BASE_DIR)
+print(PROJECT_ROOT)
+print("root", STATIC_ROOT)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- src/Swapping/urls.py
+++ src/Swapping/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path("accounts/", include("django.contrib.auth.urls"))  # new
 ]
+
+urlpatterns += staticfiles_urlpatterns()
