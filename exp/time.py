962df9ff23090419bc334b87578e4dd6ae4ea197
Luísa Araújo <lmpa.pt@gmail.com>
2022-06-18 15:44:40 +0100

    deleted exp

--- exp/time.py
+++
@@ -1,8 +0,0 @@
-import time
-
-
-start = time.time()
-time.sleep(1)
-end = time.time()
-
-print(start)
--- exp/token.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-def tokenGenerator(id):
-    token = str(id) + str(datetime.datetime.now().timestamp())
-    return token
-
-tokenGenerator(5)
